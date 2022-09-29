7bdaca378200f0e609980d18f6d393d4e55a67f9
Virvon <dan130205@gmail.com>
2022-09-29 22:20:19 +0300

    Update Program.cs

--- crystal store/Program.cs
+++ crystal store/Program.cs
@@ -10,15 +10,17 @@ namespace crystal_store
     {
         static void Main(string[] args)
         {
-            int moneyCount;
-            int crystalsCount;
+            int money;
+            int crystals;
             int crystalCost = 10;
+
             Console.WriteLine("Добро пожаловать в магазин кристаллов! Только сегодня один кристалл стоит " + crystalCost + " золота. Сколько у вас его есть?");
-            moneyCount = Convert.ToInt32(Console.ReadLine());
+            money = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Хорошо, какое количество кристаллов вам надо?");
-            crystalsCount = Convert.ToInt32(Console.ReadLine());
-            moneyCount -= crystalsCount * crystalCost;
-            Console.WriteLine("После покупки у вас в сумке " + moneyCount + " монет и " + crystalsCount + " кристаллов");
+            crystals = Convert.ToInt32(Console.ReadLine());
+            money -= crystals * crystalCost;
+
+            Console.WriteLine("После покупки у вас в сумке " + money + " монет и " + crystals + " кристаллов");
             Console.ReadKey();
         }
     }
